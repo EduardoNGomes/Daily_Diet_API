f9111b5160be1b6cd8ea9d4b9fd27a1c71fd72e8
Eduardo N Gomes <eduardo_n_gomes.dev@hotmail.com>
2023-06-07 16:22:29 -0300

    feature:Install fastify-multer

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,13 +1,19 @@
 import { FastifyInstance } from 'fastify'
 import { z } from 'zod'
+import { upload } from '../configs/multer'
 
 export async function userRouter(app: FastifyInstance) {
-  app.post('/users', (request, reply) => {
-    const bodySchema = z.object({
-      name: z.string(),
-    })
-    const { name } = bodySchema.parse(request.body)
+  app.post(
+    '/users',
+    { preHandler: upload.single('file') },
+    (request, reply) => {
+      console.log(request.file)
+      const bodySchema = z.object({
+        name: z.string(),
+      })
+      const { name } = bodySchema.parse(request.body)
 
-    return reply.status(200).send(name)
-  })
+      return reply.status(200).send(name)
+    },
+  )
 }
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,14 @@
 import fastify from 'fastify'
 import { env } from './env'
+import multer from 'fastify-multer'
 import { userRouter } from './routes/user'
+
 const app = fastify()
 
+// Uploads file
+app.register(multer.contentParser)
+
+// Routes
 app.register(userRouter)
 
 app.listen({ port: env.PORT }).then(() => {
